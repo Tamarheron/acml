4b0da1701cad243b66f0471e3adfa87019199a69
Elizabeth Barnes <beth.m.barnes@gmail.com>
2016-09-07 16:34:52 +0100

    changes from terry's branch

--- regressiondata.py
+++ regressiondata.py
@@ -7,87 +7,92 @@ class RegressionData:
     Manage antigenic cartography data for regression analysis.
 
     @param filename: The C{str} name of the CSV file containing the data.
+    @param dropFirstRow: Whether to drop the first row of the data
     """
 
-    def __init__(self, filename, dropFirstRow=False):
+    def __init__(self, filename, dropFirstRow=False, strain1ColHeading='AG1', strain2ColHeading='AG2'):
         df = pd.read_csv(filename)
         if dropFirstRow:
             self.df = df.drop(0)
         else:
             self.df = df
 
-        self.separate_dates('AG1', 'AG2')
-        self.add_clusters('strain_1_dates', 'strain_2_dates')
+        #Add two columns containing 4 digit dates for each strain
+        self.separate_dates(strain1ColHeading, strain2ColHeading)
 
-    def add_clusters(self, date1, date2):
+        #Add two columns containing the cluster label for each strain
+        self.add_clusters()
+
+    def add_clusters(self):
         """
-        Add clusters....
+        Add columns that contain cluster labels, based on the 
+        clusters in the map here http://www.antigenic-cartography.org/
         """
-        cluster1 = []
-        cluster2 = []
+        strain_1_cluster = []
+        strain_2_cluster = []
 
-        for date in self.df[date1]:
+        for date in self.df['strain_1_dates']:
             if date < 1972:
-                cluster1.append('HK68')
+                strain_1_cluster.append('HK68')
             elif date < 1975:
-                    cluster1.append('EN72')
+                    strain_1_cluster.append('EN72')
             elif date < 1977:
-                    cluster1.append('VI75')
+                    strain_1_cluster.append('VI75')
             elif date < 1979:
-                    cluster1.append('TX77')
+                    strain_1_cluster.append('TX77')
             elif date < 1987:
-                    cluster1.append('BK79')
+                    strain_1_cluster.append('BK79')
             elif date < 1989:
-                    cluster1.append('SI87')
+                    strain_1_cluster.append('SI87')
             elif date < 1992:
-                    cluster1.append('BE89')
+                    strain_1_cluster.append('BE89')
             elif date < 1995:
-                    cluster1.append('BE92')
+                    strain_1_cluster.append('BE92')
             elif date < 1997:
-                    cluster1.append('WU95')
+                    strain_1_cluster.append('WU95')
             elif date < 2002:
-                    cluster1.append('SY97')
+                    strain_1_cluster.append('SY97')
             else:
-                    cluster1.append('FU02')
+                    strain_1_cluster.append('FU02')
 
-        for date in self.df[date2]:
+        for date in self.df['strain_1_dates']:
             if date < 1972:
-                cluster2.append('HK68')
+                strain_2_cluster.append('HK68')
             elif date < 1975:
-                    cluster2.append('EN72')
+                    strain_2_cluster.append('EN72')
             elif date < 1977:
-                    cluster2.append('VI75')
+                    strain_2_cluster.append('VI75')
             elif date < 1979:
-                    cluster2.append('TX77')
+                    strain_2_cluster.append('TX77')
             elif date < 1987:
-                    cluster2.append('BK79')
+                    strain_2_cluster.append('BK79')
             elif date < 1989:
-                    cluster2.append('SI87')
+                    strain_2_cluster.append('SI87')
             elif date < 1992:
-                    cluster2.append('BE89')
+                    strain_2_cluster.append('BE89')
             elif date < 1995:
-                    cluster2.append('BE92')
+                    strain_2_cluster.append('BE92')
             elif date < 1997:
-                    cluster2.append('WU95')
+                    strain_2_cluster.append('WU95')
             elif date < 2002:
-                    cluster2.append('SY97')
+                    strain_2_cluster.append('SY97')
             else:
-                    cluster2.append('FU02')
+                    strain_2_cluster.append('FU02')
 
-        self.df['cluster1'] = pd.Series(cluster1, index=self.df.index)
-        self.df['cluster2'] = pd.Series(cluster2, index=self.df.index)
+        self.df['strain_1_cluster'] = pd.Series(strain_1_cluster, index=self.df.index)
+        self.df['strain_2_cluster'] = pd.Series(strain_2_cluster, index=self.df.index)
 
-    def separate_dates(self, col1, col2):
+    def separate_dates(self, strain1ColHeading, strain2ColHeading):
         """
         Add columns to data frame with strain dates separated into their
         components. Adjust years from 2-digit strings to integers that
         include the century.
 
-        @param col1: The C{str} strain1 label column heading in self.df.
-        @param col2: The C{str} strain2 label column heading in self.df.
+        @param strain1ColHeading: The C{str} strain1 label column heading in self.df.
+        @param strain2ColHeading: The C{str} strain2 label column heading in self.df.
         """
-        strain_1_labels = list(self.df[col1])
-        strain_2_labels = list(self.df[col2])
+        strain_1_labels = list(self.df[strain1ColHeading])
+        strain_2_labels = list(self.df[strain2ColHeading])
 
         strain_1_dates = []
         strain_2_dates = []
@@ -113,37 +118,44 @@ class RegressionData:
         self.df['strain_2_dates'] = pd.Series(strain_2_dates,
                                               index=self.df.index)
 
-    def cv(self, cluster1, cluster2):
+    def cv(self, strain_1_cluster, strain_2_cluster):
         """
         Create a cross validation set.
 
-        The training set consists of all pairs where neither strain is a
-        member of a particular cluster. The test set consists of all pairs
+        The training sets consists of all pairs where neither strain is a
+        member of a particular cluster. The test sets consists of all pairs
         where at least one strain is a member of that cluster
 
-        @return: A cross-validation something...
+        @params: headings of the columns which contain the labels (e.g. cluster names) that
+        you want to split the data on
+
+        @return: A cross-validation object, which is a list of train-test tuples 
+        which contain the indices of the rows of the dataframe to be used in the 
+        training and test data. This can be passed to scikit-learn's cross validation
+        methods
         """
         df = self.df
         return [(np.where(
-            (df[cluster1] != label) & (df[cluster2] != label))[0],
-               np.where((df[cluster1] == label) & (df[cluster2] == label))[0])
-              for label in np.unique(df[cluster1])]
+            (df[strain_1_cluster] != label) & (df[strain_2_cluster] != label))[0],
+               np.where((df[strain_1_cluster] == label) & (df[strain_2_cluster] == label))[0])
+              for label in np.unique(df[strain_1_cluster])]
 
     def splitByYear(self, year):
         """
-        Split self.df by a given year.
+        Split self.df into training and test sets by a given year.
+
+        The training sets consist of all pairs where both strains 
+        are up to the given date. The test set consists of all pairs
+        where at least one strain is after the given date
 
         @return: A C{dict} with keys:
 
-            'X': the X data...
+            'train'
+            'test'
         """
 
         df = self.df
-        X = df.drop(['AG1', 'AG2', 'AG-DIST', 'NUM-MUTATIONS',
-                     'strain_1_dates', 'strain_2_dates', 'cluster1',
-                     'cluster2'], axis=1)
 
-        y = df['AG-DIST']
 
         train = df.loc[((df['strain_1_dates'] <= year) &
                         (df['strain_2_dates'] <= year))]
@@ -151,27 +163,8 @@ class RegressionData:
         test = df.loc[((df['strain_1_dates'] > year) |
                        (df['strain_2_dates'] > year))]
 
-        xtrain = train.drop(
-            ['AG1', 'AG2', 'AG-DIST', 'NUM-MUTATIONS',
-             'strain_1_dates', 'strain_2_dates', 'cluster1', 'cluster2'],
-            axis=1)
-
-        ytrain = train['AG-DIST']
-
-        xtest = test.drop(
-            ['AG1', 'AG2', 'AG-DIST', 'NUM-MUTATIONS',
-             'strain_1_dates', 'strain_2_dates', 'cluster1', 'cluster2'],
-            axis=1)
-
-        ytest = test['AG-DIST']
-
         return {
-            'X': X,
-            'y': y,
+
             'train': train,
-            'test': test,
-            'xtrain': xtrain,
-            'ytrain': ytrain,
-            'xtest': xtest,
-            'ytest': ytest,
+            'test': test
         }
